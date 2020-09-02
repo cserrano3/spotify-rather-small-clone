dadb370a094d4fc5eabadb6fbe4a215a626d6fbe
carolina <you@example.com>
2020-09-02 00:42:01 -0300

    change folder structure for tests

--- src/test/kotlin/spotifylikeclone/ServerApplicationTests.kt
+++ src/test/kotlin/spotifylikeclone/ServerApplicationTests.kt
@@ -1,10 +1,10 @@
-package com.okta.kotlin
+package spotifylikeclone
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ResourceServerApplicationTests {
+class ServerApplicationTests {
 
 	@Test
 	fun contextLoads() {
